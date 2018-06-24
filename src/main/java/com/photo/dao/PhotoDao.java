158e045528fc01d05e43d7ccab4b16bc61c00d4e
亮 <liang.wang@wormpex.com>
2018-06-24 15:06:10 +0800

    add

---
+++ src/main/java/com/photo/dao/PhotoDao.java
@@ -0,0 +1,5 @@
+package com.photo.dao;
+
+public interface PhotoDao {
+
+}
---
+++ src/main/java/com/photo/dao/impl/PhotoDaoImpl.java
@@ -0,0 +1,10 @@
+package com.photo.dao.impl;
+
+import org.springframework.stereotype.Repository;
+
+import com.photo.dao.PhotoDao;
+
+@Repository
+public class PhotoDaoImpl implements PhotoDao {
+
+}
