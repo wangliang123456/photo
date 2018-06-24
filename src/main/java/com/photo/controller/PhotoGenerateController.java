32eae56e9e4d19220d9890927a248d39647d09e6
亮 <liang.wang@wormpex.com>
2018-06-24 14:36:29 +0800

    add

---
+++ src/main/java/com/photo/controller/PhotoGenerateController.java
@@ -0,0 +1,17 @@
+package com.photo.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/photo")
+public class PhotoGenerateController {
+	
+	@PostMapping("/generate/v1")
+	public void generateIDPhoto() {
+		
+	}
+
+	
+}
