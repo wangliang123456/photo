6e03bbbcedbcb6855b3ef222c8117005010985ab
亮 <liang.wang@wormpex.com>
2018-07-07 08:19:07 +0800

    add

---
+++ src/main/java/com/photo/controller/ImageSpecController.java
@@ -0,0 +1,15 @@
+package com.photo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ImageSpecController {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ImageSpecController.class);
+	
+	
+	
+
+}
